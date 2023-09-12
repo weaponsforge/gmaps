c448768bcac7f461319fb8d87fee8ac7c42d8e3e
weaponsforge <forgeweapons@gmail.com>
2023-09-12 18:51:01 +0800

    chore: fetch addresses using google maps nearbysearch api on frontend

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import MapDraw from './map-draw/map-draw.js'
 const start = () => {
   const map = new MapDraw({
     mapId: 'map',
+    gmapId: 'gmap',
     baseMapUrl: 'mapbox://styles/mapbox/satellite-v9'
   })
 
