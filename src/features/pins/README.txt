24dece55345bff34fad980656098e9edf9e59a1d
weaponsforge <forgeweapons@gmail.com>
2023-10-07 04:26:16 +0800

    chore: log clicked marker coords

---
+++ src/features/pins/README.txt
@@ -0,0 +1,9 @@
+Review/check other Leaflet plugins:
+
+https://github.com/Leaflet/Leaflet.Editable
+https://github.com/kartena/Leaflet.EditableHandlers
+https://github.com/iatkin/leaflet-svgicon
+
+Test Leaflet components for overrides:
+https://github.com/Leaflet/Leaflet/blob/main/src/layer/marker/Icon.js
+https://github.com/Leaflet/Leaflet/blob/main/src/layer/marker/Marker.js
--- src/features/pins/pin.js
+++ src/features/pins/pin.js
@@ -5,18 +5,42 @@ import { LeafletGoogleMap } from '../../lib/maps/leaflet'
 import './assets/css/basic-leaflet.css'
 
 class CustomIcon extends LeafletGoogleMap {
+  // Leaflet custom icon https://leafletjs.com/examples/custom-icons/
+  icon
+
+  // Leaflet divIcon https://leafletjs.com/reference.html#divicon
+  divIcon
+
   constructor (params) {
     super(params)
 
     /* eslint-disable no-undef */
     this.bindMapClick()
     this.initIcon()
+    this.initDivIcon()
 
     const center = this.map.getCenter()
     const marker = L.marker(center, { icon: this.icon }).addTo(this.map)
 
     marker.addEventListener('click', (e) => {
       console.log('icon clicked')
+
+      // Get the pixel coordinates of the click event relative to the marker's icon
+      const iconSize = this.icon.options.iconSize
+      const clickedPoint = this.map.latLngToContainerPoint(e.latlng)
+      const markerPosition = this.map.latLngToContainerPoint(marker.getLatLng())
+      const relativeClickPoint = clickedPoint.subtract(markerPosition)
+
+      // Define the threshold for the bottom area (adjust as needed)
+      const bottomThreshold = iconSize[1] * 0.7
+
+      console.log(clickedPoint, iconSize)
+
+      // Check if the click falls within the bottom portion of the marker
+      if (relativeClickPoint.y > bottomThreshold) {
+        // Your custom click event handler code here for the bottom part
+        console.log('Marker clicked at the bottom!')
+      }
     })
   }
 
@@ -24,11 +48,26 @@ class CustomIcon extends LeafletGoogleMap {
 
   }
 
+  /**
+   * Creates a Leaflet icon
+   */
   initIcon () {
     this.icon = L.icon({
       iconUrl: './features/pins/assets/img/red-icon-128.png',
       iconSize: [128, 128],
       iconAnchor: [64, 64]
+    })
+  }
+
+  /**
+   * Creates a Leaflet DIV icon
+   */
+  initDivIcon () {
+    this.divIcon = L.divIcon({
+      className: 'custom-marker',
+      html: '<div class="marker-content"></div>',
+      iconSize: [128, 128], // Set the size of your custom icon
+      iconAnchor: [64, 128] // Set the anchor point to the middle of the bottom edge
     })
   }
 }
